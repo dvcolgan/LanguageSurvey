7ab3e0c38d429a028614cfcaf6c8690736a07777
David Colgan <dvcolgan@gmail.com>
2010-09-09 11:53:52 -0400

    Human command line interface almost done

--- Python/farkle.py
+++ Python/farkle.py
@@ -1,52 +1,52 @@
 import random
 
 
-
-
-
 class Dice(object):
-#add the business rules to this class
     def __init__(self):
         self.score = 0
         self.set_aside = []
-        self.remaining = [Die(), Die(), Die(), Die(), Die(), Die()]
+        self.remaining = [0, 0, 0, 0, 0, 0]
 
     def get_score(self):
         return self.score
 
     def get_set_aside(self):
-        return [die.get_value() for die in self.set_aside]
+        return [die for die in self.set_aside]
 
     def get_remaining(self):
-        return [die.get_value() for die in self.remaining]
+        return [die for die in self.remaining]
 
     def roll(self):
-        for die in self.remaining:
-            die.roll()
+        self.remaining = [random.randint(1,6) for die in self.remaining]
+
+    def is_valid_set_aside(self, dice_values):
+        for die_value in dice_values:
+            found = False
+            for die in self.remaining:
+                if die == die_value:
+                    found = True
+            if found == False:
+                return False
+        return True
+
+    def dice_combination_value(self, dice_values):
+        return 100
 
     def move_to_set_aside(self, dice_values):
-        if !self.is_valid_set_aside(dice_values):
+        if not self.is_valid_set_aside(dice_values):
             raise InvalidSetAsideException()
 
-        for i in reversed(sorted(dice_indices)):
-            self.set_aside.append(self.remaining[i])
-            del(self.remaining[i])
+        value = self.dice_combination_value(dice_values)
+        if value == 0:
+            raise InvalidSetAsideException()
+
+        for die_value in dice_values:
+            self.set_aside.append(die_value)
+            self.remaining.remove(die_value)
 
 
 class InvalidSetAsideException(Exception):
     pass
-
-class Die(object):
-    def __init__(self):
-        self.value = 0
-
-    def roll(self):
-        self.value = random.randint(1,6)
-
-    def get_value(self):
-        return self.value
-
-
 
 
 class Player(object):
@@ -57,20 +57,28 @@ class HumanPlayer(Player):
     def take_turn(self, dice):
         while True:
             dice.roll()
-            remaining = ' '.join([str(die) for die in dice.get_remaining()])
-            set_aside = ' '.join([str(die) for die in dice.get_set_aside()])
-
-            print "\n\nSet Aside:\n", set_aside
-            print "Turn score: ", dice.get_score()
-            print "\nYou roll the dice:\n", remaining
-            choices = raw_input("\nIndicate the dice you want to set aside by entering their nuumbers separated by spaces, or enter nothing to stop.\n")
+            choices = self.farkle_prompt(dice)
             if choices != '':
                 try:
                     dice.move_to_set_aside([int(choice) for choice in choices.split()])
+                    print "\n"*64
                 except InvalidSetAsideException as e:
+                    print "\n"*64
                     print "That set aside is not valid."
             else:
                 return dice
+
+
+    def clear_screen(self):
+        print "\n"*64
+
+    def farkle_prompt(self, dice):
+        remaining = ' '.join([str(die) for die in dice.get_remaining()])
+        set_aside = ' '.join([str(die) for die in dice.get_set_aside()])
+        print "\n\nSet Aside:\n", set_aside
+        print "Turn score: ", dice.get_score()
+        print "\nYou roll the dice:\n", remaining
+        return raw_input("\nIndicate the dice you want to set aside by entering their numbers separated by spaces, or enter nothing to stop.\n")
 
 
 class AIPlayer(Player):
